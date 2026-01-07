2076b2c743ee3e46a5acd9adce82de519223be06
emarte369 <emmanuelmarte@gmail.com>
2026-01-06 21:04:49 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {}
+    // This allows process.env.API_KEY to be readable in the browser
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   },
   server: {
     port: 3000,
